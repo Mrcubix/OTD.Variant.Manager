443d2d2c860235639ae2f8a516aa4819c6af6eb3
Mrcubix <tintamare91@gmail.com>
2024-02-18 20:23:29 +0100

    Initial Commit

---
+++ OTD.Variant.Manager.UX/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace OTD.Variant.Manager.UX.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ OTD.Variant.Manager.UX/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace OTD.Variant.Manager.UX.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
