afb10cd1830ff4f7e98f4dcb50e372fdb2e4c9e8
Mrcubix <tintamare91@gmail.com>
2024-02-19 21:25:36 +0100

    Switched from ReactiveUI to comunityToolkit.mvvm

--- OTD.Variant.Manager.UX/ViewModels/ViewModelBase.cs
+++ OTD.Variant.Manager.UX/ViewModels/ViewModelBase.cs
@@ -1,7 +1,7 @@
-﻿using ReactiveUI;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace OTD.Variant.Manager.UX.ViewModels;
 
-public class ViewModelBase : ReactiveObject
+public class ViewModelBase : ObservableObject
 {
 }
