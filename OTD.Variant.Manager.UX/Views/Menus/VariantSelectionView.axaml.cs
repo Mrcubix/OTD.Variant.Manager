15490d9772d728bb61a27a426fe63957a1304aa3
Gess1t <tintamare91@gmail.com>
2024-02-20 20:21:31 +0100

    - Add Variant Selection View, - Make it the next page of the Device Selecxtion View

---
+++ OTD.Variant.Manager.UX/Views/Menus/VariantSelectionView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace OTD.Variant.Manager.UX.Views.Menus;
+
+public partial class VariantSelectionView : UserControl
+{
+    public VariantSelectionView()
+    {
+        InitializeComponent();
+    }
+}
